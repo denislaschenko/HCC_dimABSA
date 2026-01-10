a5e4fa87d312c700d36e8fb515b40dc8c856f788
Albert8559 <albert.korotyk@tum.de>
2026-01-10 16:41:05 +0100

    Update extract_aspects_category.py

--- src/subtask_3/extract_aspects_category.py
+++ src/subtask_3/extract_aspects_category.py
@@ -3,7 +3,7 @@ import argparse
 import re
 import torch
 import numpy as np
-from sentence_transformers import SentenceTransformer, util
+import util
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
